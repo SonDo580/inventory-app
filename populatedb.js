f0a340dbe6cab1b996ec8146535b88d0b59473b4
Son Do <dominhson580@gmail.com>
2022-12-27 21:35:43 +0700

    Create more items when populate database

--- populatedb.js
+++ populatedb.js
@@ -91,6 +91,99 @@ function createItems() {
         callback
       );
     },
+    function (callback) {
+      createItem(
+        "I SPY MATCH",
+        "Description",
+        9.99,
+        10,
+        categories[0],
+        callback
+      );
+    },
+    function (callback) {
+      createItem(
+        "LEGO STAR WAR ADVENT",
+        "Description",
+        49.99,
+        10,
+        categories[1],
+        callback
+      );
+    },
+    function (callback) {
+      createItem(
+        "DARK TROOPER ATTACK",
+        "Description",
+        49.99,
+        10,
+        categories[1],
+        callback
+      );
+    },
+    function (callback) {
+      createItem("OLIVIA", "Description", 18.99, 10, categories[2], callback);
+    },
+    function (callback) {
+      createItem(
+        "DAZZLE DUCKIE",
+        "Description",
+        7.99,
+        10,
+        categories[3],
+        callback
+      );
+    },
+    function (callback) {
+      createItem(
+        "COLOR BY NUMBER",
+        "Description",
+        3.99,
+        10,
+        categories[4],
+        callback
+      );
+    },
+    function (callback) {
+      createItem(
+        "MAGNETIC WOOD BLOCKS",
+        "Description",
+        39.99,
+        10,
+        categories[5],
+        callback
+      );
+    },
+    function (callback) {
+      createItem(
+        "LIFTING BRIDGE",
+        "Description",
+        32.99,
+        10,
+        categories[6],
+        callback
+      );
+    },
+    function (callback) {
+      createItem(
+        "ORBITING SOLAR SYSTEM",
+        "Description",
+        29.99,
+        10,
+        categories[7],
+        callback
+      );
+    },
+    function (callback) {
+      createItem(
+        "MINI PLAY GOLF GAME",
+        "Description",
+        39.99,
+        10,
+        categories[8],
+        callback
+      );
+    },
   ]);
 }
 
