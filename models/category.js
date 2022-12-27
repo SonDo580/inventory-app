197f7b571e26afc43395d727030ee0301203e014
Son Do <dominhson580@gmail.com>
2022-12-27 15:51:56 +0700

    Remove maxLength validator

--- models/category.js
+++ models/category.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   name: { type: String, required: true, maxLength: 50 },
-  description: { type: String, required: true, maxLength: 200 },
+  description: { type: String, required: true },
 });
 
 CategorySchema.virtual("url").get(function () {
