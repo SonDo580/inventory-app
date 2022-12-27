93c9b12857e9b9882d105cc5e80983c917649bc2
Son Do <dominhson580@gmail.com>
2022-12-27 15:31:47 +0700

    Add virtual property 'url'

--- models/category.js
+++ models/category.js
@@ -6,3 +6,7 @@ const CategorySchema = new Schema({
   name: { type: String, required: true, maxLength: 50 },
   description: { type: String, required: true, maxLength: 200 },
 });
+
+CategorySchema.virtual("url").get(function () {
+  return `/category/${this._id}`;
+});
