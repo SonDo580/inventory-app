44b6d39de0333c89a4e99fa16c152c8205be87a0
Son Do <dominhson580@gmail.com>
2022-12-28 10:09:55 +0700

    Define item route handlers

---
+++ routes/item.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const itemController = require("../controllers/itemController");
+
+router.get("/", itemController.item_list);
+
+router.get("/create", itemController.item_create_get);
+
+router.post("/create", itemController.item_create_post);
+
+router.get("/:id/update", itemController.item_update_get);
+
+router.post("/:id/update", itemController.item_update_post);
+
+router.get("/:id/delete", itemController.item_delete_get);
+
+router.post("/:id/delete", itemController.item_delete_post);
+
+router.get("/:id", itemController.item_detail);
+
+module.exports = router;
