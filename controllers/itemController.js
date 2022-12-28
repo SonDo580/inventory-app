a1febdb07ee870177c565b68db08cc6876bd54b9
Son Do <dominhson580@gmail.com>
2022-12-28 09:27:21 +0700

    Define and export controller functions

---
+++ controllers/itemController.js
@@ -0,0 +1,33 @@
+const Item = require("../models/item");
+
+exports.item_list = (req, res) => {
+  res.send("Item List");
+};
+
+exports.item_detail = (req, res) => {
+  res.send(`Item Detail: ${req.params.id}`);
+};
+
+exports.item_create_get = (req, res) => {
+  res.send("Create Item GET");
+};
+
+exports.item_create_post = (req, res) => {
+  res.send("Create Item POST");
+};
+
+exports.item_update_get = (req, res) => {
+  res.send("Update Item GET");
+};
+
+exports.item_update_post = (req, res) => {
+  res.send("Update Item POST");
+};
+
+exports.item_delete_get = (req, res) => {
+  res.send("Delete item GET");
+};
+
+exports.item_delete_post = (req, res) => {
+  res.send("Delete Item POST");
+};
