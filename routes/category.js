b7cc579da3e2a102ea82df2175ee39ba04c7521e
Son Do <dominhson580@gmail.com>
2022-12-28 10:08:25 +0700

    Define category route handlers

--- routes/category.js
+++ routes/category.js
@@ -2,3 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const categoryController = require("../controllers/categoryController");
+
+router.get("/", categoryController.category_list);
+
+router.get("/create", categoryController.category_create_get);
+
+router.post("/create", categoryController.category_create_post);
+
+router.get("/:id/update", categoryController.category_update_get);
+
+router.post("/:id/update", categoryController.category_update_post);
+
+router.get("/:id/delete", categoryController.category_delete_get);
+
+router.post("/:id/delete", categoryController.category_delete_post);
+
+router.get("/:id", categoryController.category_detail);
+
+module.exports = router;
