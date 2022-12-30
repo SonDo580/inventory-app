6e2df7bbf82bd2b8e4939700cc6fa460c432cbbe
Son Do <dominhson580@gmail.com>
2022-12-30 13:57:24 +0700

    Handle category update request

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -77,9 +77,51 @@ exports.category_update_get = (req, res, next) => {
   });
 };
 
-exports.category_update_post = (req, res) => {
-  res.send("Update Category POST");
-};
+exports.category_update_post = [
+  // Validate and sanitize input
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create new category with sanitized input
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+      _id: req.params.id, // This is necessary: use the same _id property
+    });
+
+    // Render the form again if there are errors
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        title: "Update Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Update category if data is valid
+    Category.findByIdAndUpdate(
+      req.params.id,
+      category,
+      {},
+      (err, updatedCategory) => {
+        if (err) {
+          return next(err);
+        }
+
+        // Redirect to category list page
+        res.redirect("/category");
+      }
+    );
+  },
+];
 
 exports.category_delete_get = (req, res) => {
   res.send("Delete Category GET");
