e62bf9987dc7da2bb75957c0031747f0e311a465
Son Do <dominhson580@gmail.com>
2022-12-30 13:47:57 +0700

    Hanlde category create post request

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -21,9 +21,45 @@ exports.category_create_get = (req, res) => {
   });
 };
 
-exports.category_create_post = (req, res) => {
-  res.send("Create Category POST");
-};
+exports.category_create_post = [
+  // Validate and sanitize input
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("description", "Description must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  // Process request
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    // Create new category with sanitized input
+    const category = new Category({
+      name: req.body.name,
+      description: req.body.description,
+    });
+
+    // Render the form again if there are errors
+    if (!errors.isEmpty()) {
+      res.render("category_form", {
+        title: "Create Category",
+        category: category,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Save category if data is valid
+    category.save((err) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Redirect to category list page
+      res.redirect("/category");
+    });
+  },
+];
 
 exports.category_update_get = (req, res) => {
   res.send("Update Category GET");
